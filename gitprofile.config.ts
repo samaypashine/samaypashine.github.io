a07ae210a2b11fc9395d6dffb7d2a662ab637b52
Samay Pashine <51475380+samaypashine@users.noreply.github.com>
2024-02-03 22:16:45 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -164,7 +164,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'nord',
+    defaultTheme: 'customTheme',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
